fa268e467a463656b6538b9f6fefa422559d68f8
thomasranieri <thomasranieri@users.noreply.github.com>
2022-05-19 14:33:35 +0930

    Still trying to get canvas running in Docker, on Windows

--- script/docker_dev_setup_windows.ps1
+++ script/docker_dev_setup_windows.ps1
@@ -1,12 +1,15 @@
 $ErrorActionPreference = "Stop"
 Set-Location ..
-Set-Content -Path '.env' -Value '$Env:COMPOSE_FILE=docker-compose.yml:docker-compose.override.yml'
+Set-Content -Path '.env' -Value 'COMPOSE_FILE=docker-compose.yml;docker-compose.override.yml'
 Copy-Item docker-compose/config/*.yml config/
 Copy-Item config/docker-compose.override.yml.example docker-compose.override.yml
 docker-compose up --build -d
 #docker-compose exec web bash
+#docker-compose run --no-deps --rm web bash
+#docker-compose exec -u 0 web bash
 docker-compose run --no-deps --rm web touch Gemfile.lock
 docker-compose run --no-deps --rm web ./script/install_assets.sh -c bundle
 docker-compose run --no-deps --rm web ./script/install_assets.sh -c yarn
-docker-compose run --no-deps --rm web ./script/install_assets.sh -c compile
-#docker-compose exec web bundle exec rake canvas:compile_assets_dev
+docker-compose run --no-deps --rm web ./script/install_assets.sh -c compile #takes 15-20 minutes on my computer
+docker-compose run --no-deps --rm web bundle exec rake db:migrate RAILS_ENV=development
+docker-compose run --no-deps --rm web bundle exec rake db:initial_setup
