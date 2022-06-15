a16c29576043083579db95bb2358d2ffc931c744
Thomas Ranieri <thomasranieri@users.noreply.github.com>
2022-06-15 13:06:33 +0930

    Canvas now works in Docker on Windows, but with long startup time

--- script/docker_dev_setup_windows.ps1
+++ script/docker_dev_setup_windows.ps1
@@ -1,12 +1,14 @@
 $ErrorActionPreference = "Stop"
-Set-Location ..
-Set-Content -Path '.env' -Value 'COMPOSE_FILE=docker-compose.yml;docker-compose.override.yml'
+Set-Location $PSScriptRoot\..
+Set-Content -Path '.env' -Value "COMPOSE_FILE=docker-compose.yml;docker-compose.override.yml`nPASSENGER_STARTUP_TIMEOUT=3000"
 Copy-Item docker-compose/config/*.yml config/
 Copy-Item config/docker-compose.override.yml.example docker-compose.override.yml
+#Then add APP_DOMAIN: localhost to environment of jobs
+#and add to web:
+# ports:
+# - 127.0.0.1:80:80
+
 docker-compose up --build -d
-#docker-compose exec web bash
-#docker-compose run --no-deps --rm web bash
-#docker-compose exec -u 0 web bash
 docker-compose run --no-deps --rm web touch Gemfile.lock
 docker-compose run --no-deps --rm web ./script/install_assets.sh -c bundle
 docker-compose run --no-deps --rm web ./script/install_assets.sh -c yarn
